739cbc7d369a8f5f17b4c26fe65ba97d80d117b5
Abinesh <abinesh@xcelcorp.com>
2022-10-12 16:17:32 +0530

    password encryption added

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const routes = express.Router();
 const user = require("../models/users_module");
+const bcrypt = require("bcrypt");
 
 routes.post("/", async (req, res) => {
   const { userName, password } = req.body;
-  const usersschema = new user({
-    userName: userName,
-    password: password,
-  });
-  await usersschema
-    .save()
-    .then((data) => {
-      res.json({ DATA: data });
+  await user
+    .findOne({ userName: userName })
+    .then((user) => {
+      if (user) throw "USER ALREADY EXIST";
+      bcrypt.hash(password, 7).then((hash) => {
+        const usersschema = new user({
+          userName: userName,
+          password: hash,
+        });
+        usersschema.save().then((data) => {
+          res.json({ DATA: data });
+        });
+      });
     })
     .catch((err) => {
       res.json({ ERROR: err });
     });
 });
 
-routes.get("/login", async (req, res) => {
+routes.post("/login", async (req, res) => {
   const { userName, password } = req.body;
   await user
-    .findOne({ userName: userName, password: password })
-    .then((data) => {
-      if (data) {
-        res.json({ AUTH: true });
-      } else {
-        res.json({ AUTH: false });
-      }
+    .findOne({ userName: userName })
+    .then((user) => {
+      if (!user) throw "USER NOT FOUND";
+      bcrypt.compare(password, user.password).then((ok) => {
+        res.json({ AUTH: ok ? true : false });
+      });
     })
     .catch((err) => {
       res.json({ ERROR: err });
